bd6efd7987d97a40235843156962b3231f086bc3
Melissa Peck <melissapeck88@gmail.com>
2020-11-21 14:28:24 -0600

    Finished with all but bonus

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -6,12 +6,9 @@ LEFT JOIN salaries s
 	on ee.emp_no = s.emp_no
 
 --List first name, last name, and hire date for employees who were hired in 1986.
-SELECT ee.hire_date from employees ee,
-	CAST(hire_date as VARCHAR(10))
-
 SELECT ee.last_name, ee.first_name, ee.hire_date
 from employees ee
-	where  ee.hire_date = 1986*
+	where  ee.hire_date between '1986-01-01' and '1986-12-31'
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 SELECT dm.dept_no, dm.emp_no, d.dept_name, ee.last_name, ee.first_name
@@ -32,7 +29,7 @@ INNER JOIN departments d
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex
 from employees
-where first_name = 'Hercules' and last_name = 'B%'
+where first_name = 'Hercules' and last_name like 'B%'
 
 --List all employees in the Sales department, including their employee number, last name, first name, and department name.
 SELECT ee.emp_no, ee.last_name, ee.first_name, d.dept_name
@@ -41,7 +38,7 @@ INNER JOIN dept_emp de
 	on ee.emp_no = de.emp_no
 INNER JOIN departments d
 	on de.dept_no = d.dept_no
-where de.dept_no = 'd007'
+where d.dept_name = 'Sales'
 
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT ee.emp_no, ee.last_name, ee.first_name, d.dept_name
@@ -50,10 +47,11 @@ INNER JOIN dept_emp de
 	on ee.emp_no = de.emp_no
 INNER JOIN departments d
 	on de.dept_no = d.dept_no
-where de.dept_no = 'd007' or de.dept_no = 'd005'
+where d.dept_name = 'Sales' or d.dept_name = 'Development'
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 SELECT last_name, COUNT(ee.last_name)
 from employees ee
 GROUP BY last_name
+ORDER BY count(ee.last_name) desc
 
